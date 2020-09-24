6fb2c85af786fc92e8da6f728b0e0c341948bb8f
CheezItMan <chris@adadevelopersacademy.org>
2020-09-23 21:45:56 -0700

    initial tests

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,2 +1,35 @@
 require_relative "test_helper"
 
+describe "fizzbuzz" do
+  it "will return [\"1\"] for 1" do
+    n = 1
+
+    answer = fizzbuzz(n)
+
+    expect(answer).must_equal ["1"]
+  end
+
+  it "will answer the example correctly" do
+    n = 15
+
+    answer = fizzbuzz(n)
+
+    expect(answer).must_equal [
+      "1",
+      "2",
+      "Fizz",
+      "4",
+      "Buzz",
+      "Fizz",
+      "7",
+      "8",
+      "Fizz",
+      "Buzz",
+      "11",
+      "Fizz",
+      "13",
+      "14",
+      "FizzBuzz"
+  ]
+  end
+end
